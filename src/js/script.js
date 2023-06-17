c74f727e2c0d1b207eb3a0021986d2dcef965484
Diogo Cesar Rodrigues <dcesar_rodrigues@hotmail.com>
2023-06-17 12:53:19 -0300

    Adicionando hover e eventos para abrir o menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+//banner principal mobile
 new Splide( '.splide.splide-mobile', {
   type   : 'loop',
   perPage: 1,
@@ -5,12 +6,25 @@ new Splide( '.splide.splide-mobile', {
   arrows: false,
 } ).mount();
 
+//banner principal desktop
 new Splide( '.splide.splide-desktop', {
   type   : 'loop',
   perPage: 1,
   pagination: true,
   arrows: false,
 } ).mount();
+
+
+// gallery dropdown
+new Splide( '.splide.gallery-dropdown-splide', {
+  type   : 'loop',
+  perPage: 4,
+  pagination: true,
+  arrows: true,
+  gap: 30,
+  perMove: 1,
+} ).mount();
+
 
 //efeito visual para o topo
 window.onscroll = function() {
@@ -21,3 +35,23 @@ window.onscroll = function() {
   header.classList.add("scroll-on") :  
   header.classList.remove("scroll-on");
 };
+
+
+// Abrindo o menu por click
+const menuItems = document.querySelectorAll(".header__menuitem");
+const dropdownMenu = document.getElementById("dropdown-gallery");
+
+// verifica cada item do menu
+menuItems.forEach(item => {
+    item.addEventListener('click', function(event) {
+      // impede que o evento atual se propague para outros elementos
+      event.stopPropagation(); 
+      dropdownMenu.style.display = 'flex';
+  });
+
+  document.addEventListener('click', function(event) {
+    if (event.target !== menuItems && !dropdownMenu.contains(event.target)) {
+      dropdownMenu.style.display = 'none';
+    }
+  });
+})
