b1ef6c3f09dedb4720df0c5345c74cb94ad31f30
Diogo Cesar Rodrigues <dcesar_rodrigues@hotmail.com>
2023-06-16 21:14:14 -0300

    Ajustes gerais de responsividade

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,23 @@
-new Splide( '.splide', {
+new Splide( '.splide.splide-mobile', {
   type   : 'loop',
   perPage: 1,
   pagination: true,
   arrows: false,
 } ).mount();
+
+new Splide( '.splide.splide-desktop', {
+  type   : 'loop',
+  perPage: 1,
+  pagination: true,
+  arrows: false,
+} ).mount();
+
+//efeito visual para o topo
+window.onscroll = function() {
+  var header = document.getElementById("header");
+  var sticky = header.offsetTop;
+
+  window.scrollY > sticky ? 
+  header.classList.add("scroll-on") :  
+  header.classList.remove("scroll-on");
+};
